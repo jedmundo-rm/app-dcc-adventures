c0ed1b09bf65c8c5bce96a97913dbd7cdc3200f3
jedmundo-rm <79552053+jedmundo-rm@users.noreply.github.com>
2026-06-05 23:37:34 -0600

    ajuste

--- js/extra-layer.js
+++ js/extra-layer.js
@@ -49,6 +49,13 @@
 
             // Escuchamos cuando los datos del video estén listos en el iPad
             videoElement.addEventListener('loadedmetadata', function() {
+
+
+            // Forzamos un sutil redibujado. Cambiar el estilo y volverlo a poner
+            // a menudo arregla los bugs de renderizado de Safari.
+            $(videoElement).css('height', 'auto');
+            $(videoElement).hide().show(0); // Forzar un reflow (redibujado)
+
                 var anchoNativo = this.videoWidth;
                 var altoNativo = this.videoHeight;
 
